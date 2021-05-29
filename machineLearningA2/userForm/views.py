cdf2e1fc997a3327ed3d2cc11a897bbe33311d71
Pranav Malaviya <pranavmalaviya2@gmail.com>
2021-05-29 18:43:57 +1000

    Upload image (#2)
    * Added mdl template
    
    * Added upload and save image locally

--- machineLearningA2/userForm/views.py
+++ machineLearningA2/userForm/views.py
@@ -1,6 +1,19 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from .forms import UploadForm
 
 # Create your views here.
-def form(request):
-    return HttpResponse('form view')
+def formView(request):
+    # formToRender = UploadForm
+    # return render(request, 'form.html', {'form': formToRender})
+    """Process images uploaded by users"""
+    if request.method == 'POST':
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            # Get the current instance object to display in the template
+            img_obj = form.instance
+            return render(request, 'form.html', {'form': form, 'img_obj': img_obj})
+    else:
+        form = UploadForm()
+    return render(request, 'form.html', {'form': form})
