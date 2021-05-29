4fcab81c869cb0fd333b015bd9b3ab5f438da577
Pranav Malaviya <pranavmalaviya2@gmail.com>
2021-05-29 18:42:21 +1000

    Added upload and save image locally

--- machineLearningA2/userForm/forms.py
+++ machineLearningA2/userForm/forms.py
@@ -1,5 +1,7 @@
 from django import forms
+from .models import Image
 
-class UploadForm(forms.Form):
-    description = forms.CharField(required=False)
-    # image = 
+class UploadForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = ('title', 'image')
--- machineLearningA2/userForm/views.py
+++ machineLearningA2/userForm/views.py
@@ -3,6 +3,17 @@ from django.shortcuts import render
 from .forms import UploadForm
 
 # Create your views here.
-def form(request):
-    formToRender = UploadForm
-    return render(request, 'form.html', {'form': formToRender})
+def formView(request):
+    # formToRender = UploadForm
+    # return render(request, 'form.html', {'form': formToRender})
+    """Process images uploaded by users"""
+    if request.method == 'POST':
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            # Get the current instance object to display in the template
+            img_obj = form.instance
+            return render(request, 'form.html', {'form': form, 'img_obj': img_obj})
+    else:
+        form = UploadForm()
+    return render(request, 'form.html', {'form': form})
