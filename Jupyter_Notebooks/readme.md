3fcaa71d9cb04ab0f0b999fa164fc3010768b451
Pranav Malaviya <pranavmalaviya2@gmail.com>
2021-06-07 03:16:32 +1000

    Integrate Machine Learning Models with django (#6)
    * Added ml models integration with django
    
    * Added Python Notebooks

---
+++ Jupyter_Notebooks/readme.md
@@ -0,0 +1,5 @@
+Following files are required in the same directory to run the jupyter notebooks:
+
+- `patch_images` folder with all the classification images
+- data_labels_extraData.csv
+- data_labels_mainData.csv
--- machineLearningA2/userForm/views.py
+++ machineLearningA2/userForm/views.py
@@ -1,19 +1,52 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from .forms import UploadForm
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import os.path
+from tensorflow.keras.preprocessing import image
+import numpy as np
 
 # Create your views here.
 def formView(request):
-    # formToRender = UploadForm
-    # return render(request, 'form.html', {'form': formToRender})
     """Process images uploaded by users"""
+    form = UploadForm(request.POST, request.FILES)
     if request.method == 'POST':
-        form = UploadForm(request.POST, request.FILES)
+        
         if form.is_valid():
             form.save()
             # Get the current instance object to display in the template
             img_obj = form.instance
-            return render(request, 'form.html', {'form': form, 'img_obj': img_obj})
-    else:
-        form = UploadForm()
+            model1 = tf.keras.models.load_model(os.path.join(os.getcwd(),'isCancerousModel\isCancerousFinalModel'))
+            pathToImage = os.path.join(os.getcwd(),'..','media','testImages',img_obj.title+'.png')
+            img = tf.keras.preprocessing.image.load_img(
+                pathToImage, target_size=(27,27)
+            )
+            img_array = tf.keras.preprocessing.image.img_to_array(img)
+            img_array = tf.expand_dims(img_array, 0)  # Create batch axis
+            predictions1 = model1.predict(img_array)
+            isCancerous = np.argmax(predictions1,axis = 1)
+            print(isCancerous)
+            model2 = tf.keras.models.load_model(os.path.join(os.getcwd(),'cellTypeModel\cellTypeFinalModel'))
+            predictions2 = model2.predict(img_array)
+            cellType = np.argmax(predictions2,axis = 1)
+            print(cellType)
+                
+            if(cellType[0] == 0):
+                cellTypeName = 'Fibroblast'
+            elif(cellType[0] == 1):
+                cellTypeName = 'Inflammatory'
+            elif(cellType[0] == 2):
+                cellTypeName = 'Epithelial'
+            elif(cellType[0] == 3):
+                cellTypeName = 'Others'
+            else:
+                cellTypeName = 'Invalid cell type'
+                
+            result = "CellType: " + str(cellType[0])  + " Name: " + cellTypeName + " isCancerous: " + str(isCancerous[0])
+
+            return HttpResponse(result, content_type="text/plain")
+            
+        else:
+            form = UploadForm()
     return render(request, 'form.html', {'form': form})
